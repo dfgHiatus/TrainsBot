b24579e21ed85d51a967fdb8fc6054f909f08f43
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-12 02:41:53 +0000

    internet

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
+import re
+import json
 import discord
+import requests
 
 discordToken = os.environ['TOKEN']
+headers = {"Username" : "rttapi_dfgHiatus",
+           "Password"  : "2d5f33850a19f59bc597b65ba2052f21810544f1"}
+endpoint =  "api.rtt.io/api/v1/json/"
 
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
+async def on_message(ctx, *, message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+    if message.content.startswith('!search'):
+        try:
+          keywords = re.split("( )", message)
+          response = json.loads(requests.get(endpoint + "/json/search/" + keywords[0],    headers))
+          msg = response(["destination"])
+          await client.send_message(message.channel, msg)
+        except Exception:
+            await client.send_message(message.channel, "Something bad happened, please try again.")
 
 @client.event
 async def on_ready():
