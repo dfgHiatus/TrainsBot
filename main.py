3bd3910119974bdd0cbe531d8cc165bd1e4fdfb4
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-12 22:33:25 +0000

    touchup

--- main.py
+++ main.py
@@ -22,24 +22,25 @@ async def on_message(message):
         try:
           print("Starting search...")
           keywords = re.split("( )", message.content)
-          # Assert message is formatted !search BMH -. ['!search','','BMH']
+          # This is cursed - Assert message is formatted !search BMH -. ['!search','','BMH']
           if len(keywords) != 3:
             await message.channel.send("Invalid input, try again")
-          print("Invoking API")
-          # Somehow, this refuses to work unless you put the api key in as a string
-          response = requests.get(endpoint + "search/" + keywords[2],
-                                  auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
-          status_code = response.status_code
-          if (status_code == 200):
-              response = json.loads(response.text)
-              for serv in response["services"]:
-                  for locDet in serv["locationDetail"].get("destination"):
-                          msg = serv["locationDetail"].get("tiploc") + ", " + serv["locationDetail"].get("description") + "'s next destinaiton is " + locDet["tiploc"] + ", " + locDet["description"] + "."
-              print("Got response from API:") 
-              print(msg)
-              await message.channel.send(msg)
-          else:
-            await message.channel.send("Bad Status from API " + str(status_code))
+          else:  
+            print("Invoking API")
+            # Somehow, this refuses to work unless you put the api key in as a string
+            response = requests.get(endpoint + "search/" + keywords[2],
+                                    auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
+            status_code = response.status_code
+            if (status_code == 200):
+                response = json.loads(response.text)
+                for serv in response["services"]:
+                    for locDet in serv["locationDetail"].get("destination"):
+                            msg = serv["locationDetail"].get("tiploc") + ", " + serv["locationDetail"].get("description") + "'s next destinaiton is " + locDet["tiploc"] + ", " + locDet["description"] + "."
+                print("Got response from API:") 
+                print(msg)
+                await message.channel.send(msg)
+            else:
+              await message.channel.send("Bad Status from API " + str(status_code))
         except Exception:
             await message.channel.send("That sign wasn't found, please try again.")
 
