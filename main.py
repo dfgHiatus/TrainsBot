86e50db35776de19ecb2351aa130636f186ee190
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-12 22:29:40 +0000

    it works heh

--- main.py
+++ main.py
@@ -3,27 +3,45 @@ import re
 import json
 import discord
 import requests
+from requests.auth import HTTPBasicAuth
 
 discordToken = os.environ['TOKEN']
-headers = {"Username" : "rttapi_dfgHiatus",
-           "Password"  : "2d5f33850a19f59bc597b65ba2052f21810544f1"}
-endpoint =  "api.rtt.io/api/v1/json/"
+# username = os.environ['USERNAME']
+# password = os.environ['PASSWORD']
+endpoint =  "https://api.rtt.io/api/v1/json/"
 
 client = discord.Client()
 
 @client.event
-async def on_message(ctx, *, message):
+async def on_message(message):
     if message.author == client.user:
         return
 
+    print(message.content)
     if message.content.startswith('!search'):
         try:
-          keywords = re.split("( )", message)
-          response = json.loads(requests.get(endpoint + "/json/search/" + keywords[0],    headers))
-          msg = response(["destination"])
-          await client.send_message(message.channel, msg)
+          print("Starting search...")
+          keywords = re.split("( )", message.content)
+          # Assert message is formatted !search BMH -. ['!search','','BMH']
+          if len(keywords) != 3:
+            await message.channel.send("Invalid input, try again")
+          print("Invoking API")
+          # Somehow, this refuses to work unless you put the api key in as a string
+          response = requests.get(endpoint + "search/" + keywords[2],
+                                  auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
+          status_code = response.status_code
+          if (status_code == 200):
+              response = json.loads(response.text)
+              for serv in response["services"]:
+                  for locDet in serv["locationDetail"].get("destination"):
+                          msg = serv["locationDetail"].get("tiploc") + ", " + serv["locationDetail"].get("description") + "'s next destinaiton is " + locDet["tiploc"] + ", " + locDet["description"] + "."
+              print("Got response from API:") 
+              print(msg)
+              await message.channel.send(msg)
+          else:
+            await message.channel.send("Bad Status from API " + str(status_code))
         except Exception:
-            await client.send_message(message.channel, "Something bad happened, please try again.")
+            await message.channel.send("That sign wasn't found, please try again.")
 
 @client.event
 async def on_ready():
@@ -32,4 +50,8 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.run(discordToken)
+try:
+  client.run(discordToken)
+# HTTPException  
+except Exception:
+  print("Rate limited")
