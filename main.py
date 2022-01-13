757684cb78cde987a4dab98d7e4c8ce21f2471f2
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-13 17:24:46 +0000

    formatting

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ from requests.auth import HTTPBasicAuth
 discordToken = os.environ['TOKEN']
 # username = os.environ['USERNAME']
 # password = os.environ['PASSWORD']
-endpoint =  "https://api.rtt.io/api/v1/json/"
+endpoint =  "https://api.rtt.io/api/v1/json/search/"
+
+outputTxt = """**Next trains at {0}**:
+
+`{1}` from `{2}`
+Expected platform: `{3}`
+This service is currently at `{4}`.
+Expected at: `{5}`"""""
+formatStringList= []
 
 client = discord.Client()
 
@@ -27,20 +35,44 @@ async def on_message(message):
             await message.channel.send("Invalid input, try again")
           else:  
             print("Invoking API")
+            print(endpoint + keywords[0])
             # Somehow, this refuses to work unless you put the secret key in as a string
-            response = requests.get(endpoint + "search/" + keywords[2],
-                                    auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
-            status_code = response.status_code
-            if (status_code == 200):
-                response = json.loads(response.text)
-                for serv in response["services"]:
-                    for locDet in serv["locationDetail"].get("destination"):
-                            msg = serv["locationDetail"].get("tiploc") + ", " + serv["locationDetail"].get("description") + "'s next destinaiton is " + locDet["tiploc"] + ", " + locDet["description"] + "."
-                print("Got response from API:") 
-                print(msg)
-                await message.channel.send(msg)
+            response = requests.get(endpoint + keywords[2],
+                        auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
+            if (response.status_code == 200):
+              response = json.loads(response.text)
+              for serv in response["services"]:
+                  for orgDet in serv["locationDetail"].get("origin"):
+                      for locDet in serv["locationDetail"].get("destination"):
+
+                          formatStringList.append(serv["locationDetail"].get("description"))
+                          formatStringList.append(orgDet["publicTime"][:2] + ':' + orgDet["publicTime"][2:])
+                          formatStringList.append(orgDet["description"])
+                          formatStringList.append(serv["locationDetail"].get("platform"))
+                          formatStringList.append(serv["locationDetail"].get("description"))
+                          formatStringList.append(serv["locationDetail"].get("realtimeDeparture")[:2] + ':' + serv["locationDetail"].get("realtimeDeparture")[2:])
+
+                          print("Got response from API:") 
+                          print(outputTxt.format(formatStringList[0],
+                                    formatStringList[1],
+                                    formatStringList[2],
+                                    formatStringList[3],
+                                    formatStringList[4],
+                                    formatStringList[5]))
+                          await message.channel.send(outputTxt.format(formatStringList[0],
+                                    formatStringList[1],
+                                    formatStringList[2],
+                                    formatStringList[3],
+                                    formatStringList[4],
+                                    formatStringList[5]))
+                          formatStringList.clear()
+
+                          break;
+                      break;
+                  break;
+                  
             else:
-              await message.channel.send("Bad Status from API " + str(status_code))
+              await message.channel.send("Bad Status from API " + str(response.status_code))
         except Exception:
             await message.channel.send("That sign wasn't found, please try again.")
 
