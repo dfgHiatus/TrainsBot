cf5ef3dcc7f3508c04e4d742b9dd9d0488a57bae
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-22 17:34:24 +0000

    cleanup

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 discordToken = os.environ['TOKEN']
-# username = os.environ['USERNAME']
-# password = os.environ['PASSWORD']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
 endpoint =  "https://api.rtt.io/api/v1/json/search/"
 
-outputTxt = """**Next trains at {0}**:
-
-`{1}` from `{2}`
-Expected platform: `{3}`
-This service is currently at `{4}`.
-Expected at: `{5}`"""""
+outputTxt = """
+`{0}` from `{1}`
+Expected platform: `{2}`
+This service is currently at `{3}`.
+Expected at: `{4}`"""""
 formatStringList= []
 
 client = discord.Client()
@@ -29,47 +28,48 @@ async def on_message(message):
     if message.content.startswith('!search'):
         try:
           print("Starting search...")
+          # Mildly cursed message assertion ['!search','','BMH']
           keywords = re.split("( )", message.content)
-          # This is cursed - Assert message is formatted !search BMH -. ['!search','','BMH']
           if len(keywords) != 3:
             await message.channel.send("Invalid input, try again")
           else:  
             print("Invoking API")
-            print(endpoint + keywords[0])
-            # Somehow, this refuses to work unless you put the secret key in as a string
+            print(endpoint + keywords[2])
             response = requests.get(endpoint + keywords[2],
-                        auth = HTTPBasicAuth('rttapi_dfgHiatus', '2d5f33850a19f59bc597b65ba2052f21810544f1'))
+                        auth = HTTPBasicAuth(username, password))
             if (response.status_code == 200):
               response = json.loads(response.text)
+              counter = 0;
+              nameCounter = 0;
+              # Print the first 5 services for this train
               for serv in response["services"]:
+                if nameCounter < 1:
+                  await message.channel.send("**Next trains at " + serv["locationDetail"].get("description") + "**")
+                  nameCounter += 1
+                if counter > 4:
+                  break;
+                else:  
                   for orgDet in serv["locationDetail"].get("origin"):
                       for locDet in serv["locationDetail"].get("destination"):
-
-                          formatStringList.append(serv["locationDetail"].get("description"))
                           formatStringList.append(orgDet["publicTime"][:2] + ':' + orgDet["publicTime"][2:])
                           formatStringList.append(orgDet["description"])
                           formatStringList.append(serv["locationDetail"].get("platform"))
                           formatStringList.append(serv["locationDetail"].get("description"))
                           formatStringList.append(serv["locationDetail"].get("realtimeDeparture")[:2] + ':' + serv["locationDetail"].get("realtimeDeparture")[2:])
-
                           print("Got response from API:") 
                           print(outputTxt.format(formatStringList[0],
                                     formatStringList[1],
                                     formatStringList[2],
                                     formatStringList[3],
-                                    formatStringList[4],
-                                    formatStringList[5]))
+                                    formatStringList[4]))
                           await message.channel.send(outputTxt.format(formatStringList[0],
                                     formatStringList[1],
                                     formatStringList[2],
                                     formatStringList[3],
-                                    formatStringList[4],
-                                    formatStringList[5]))
+                                    formatStringList[4]))
                           formatStringList.clear()
-
+                          counter += 1
                           break;
-                      break;
-                  break;
                   
             else:
               await message.channel.send("Bad Status from API " + str(response.status_code))
@@ -85,6 +85,6 @@ async def on_ready():
 
 try:
   client.run(discordToken)
-# HTTPException  
+# Will throw a HTTPException if we are being rate limited by Repl.it
 except Exception:
-  print("Rate limited")
+  print("HTTPException - Rate limited")
